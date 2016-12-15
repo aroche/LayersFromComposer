65055394463ae9cff883136f3e772840847ec188
aroche <aroche@photoherbarium.fr>
2016-12-15 10:52:58 +0100

    updated translation

---
+++ i18n/layers_from_composer.pro
@@ -0,0 +1,5 @@
+SOURCES += ../layers_from_composer.py
+SOURCES += ../layers_from_composer_dialog_base.ui
+SOURCES += ../layers_from_composer_dialog.py
+
+TRANSLATIONS += layers_from_composer_fr.ts
---
+++ i18n/layers_from_composer_fr.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="fr_FR" sourcelanguage="en">
+<context>
+    <name>LayersFromComposer</name>
+    <message>
+        <location filename="../layers_from_composer.py" line="181"/>
+        <source>&amp;Restore layers from composer</source>
+        <translation type="unfinished">&amp;Restaurer les couches depuis un composeur</translation>
+    </message>
+    <message>
+        <location filename="../layers_from_composer.py" line="171"/>
+        <source>Restore layer visibility from composer</source>
+        <translation type="unfinished">Restaurer la visibilité des couches depuis un composeur</translation>
+    </message>
+    <message>
+        <location filename="../layers_from_composer.py" line="257"/>
+        <source>Layers frm composer</source>
+        <translation type="unfinished">Couches depuis le composeur</translation>
+    </message>
+    <message>
+        <location filename="../layers_from_composer.py" line="257"/>
+        <source>Some of the layers registered in the composer do not exist anymore in the project.
+Do you want to continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../layers_from_composer.py" line="273"/>
+        <source>Error in setting layer style for </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
